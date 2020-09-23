2e55eca6c7a5fa3be4cfe9c56da08f144fc21e41
MOKTAR ALHASAPI <moktaralhasapi@gmail.com>
2020-09-23 06:33:10 +0100

    Moving parsers to a standalone file

--- src/Fakr.hs
+++ src/Fakr.hs
@@ -1,11 +1,6 @@
-{-# LANGUAGE DeriveAnyClass     #-}
-{-# LANGUAGE DeriveFunctor      #-}
 {-# LANGUAGE FlexibleContexts   #-}
 {-# LANGUAGE FlexibleInstances  #-}
-{-# LANGUAGE GADTs              #-}
-{-# LANGUAGE KindSignatures     #-}
 {-# LANGUAGE LambdaCase         #-}
-{-# LANGUAGE StandaloneDeriving #-}
 
 module Fakr (
    repl,
@@ -21,164 +16,18 @@ module Fakr (
 ) where
 
 
-import           Control.Concurrent
-import           Control.Monad
-import qualified Control.Monad.State           as S
-import           Control.Monad.Trans.State
-import           Control.Monad.Trans.Writer
-import           Data.Char
-import qualified Data.List.Zipper              as Z
-import           System.IO
-import           Text.ParserCombinators.Parsec
+import qualified Control.Monad.State as S
+import qualified Data.List.Zipper as Z
 
-data BFExpr = Increment | Decrement
-            | GoForward | GoBackward
-            | Input     | Output
-            | Loop [BFExpr] deriving (Show, Eq)
+import Control.Monad
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Writer
+import Control.Concurrent
 
-data NotDelayed = NotDelayed deriving (Show, Eq)
+import System.IO
+import Data.Char
+import Parser
 
-data UserChoice = Quit
-                | Reset         | Help
-                | Inspect       | Verbose Bool
-                | Mode Bool
-                | SlowedExecution (Either NotDelayed (Int, [BFExpr]))
-                | Code [BFExpr] | Import FilePath
-                deriving (Show, Eq)
-
-data Opt = OptHelp | File FilePath
-
-data Vals :: (* -> * -> *) where
-  Ask :: (Eq a) => a -> Vals a Int
-  Scatter :: (FromJSON b, ToJSON b, Eq k, Show k)  => [(k, b)]
-                                                   -> (b, Int)
-                                                   -> [b]
-                                                   -> Vals Double b
-  Load :: (Traversable a, S.MonadIO a, Foldable a) => Int
-                                                   -> [a Int]
-                                                   -> Vals Int Double
-
-type BFState = Z.Zipper Integer
-
-class FromJSON a where
-  deserialize :: (Show b) => a -> b -> String
-
-class ToJSON a where
-  serialize :: (Show b) => String -> b -> a
-
--- Parsing stuff
-symbols :: String
-symbols = "+-<>,."
-
-instructions :: [BFExpr]
-instructions = [
-  Increment,
-  Decrement,
-  GoBackward,
-  GoForward,
-  Input,
-  Output
- ]
-
-parseIntruction :: Parser [BFExpr]
-parseIntruction =
-  many $
-     foldl1 (<|>) (fn:
-                 zipWith (\a b -> char a >> return b) symbols instructions)
-  where
-    fn :: Parser BFExpr
-    fn = char '[' >> parseIntruction >>= \ins -> char ']' >> return (Loop ins)
-
-
-parseCode :: String -> Either ParseError [BFExpr]
-parseCode
-  = parse parseIntruction "code.bf"
-
-generiqparse :: String -> UserChoice -> Parser UserChoice
-generiqparse str out
-  = char ':' >> string str >> pure out
-
-parseVerbose, parseInspect, parseHelp, parseQuit, parseReset, parseImport :: Parser UserChoice
-parseQuit    = generiqparse "quit"    Quit
-parseInspect = generiqparse "inspect" Inspect
-parseReset   = generiqparse "reset"   Reset
-parseHelp    = generiqparse "help"    Help
-
-parseQIRH :: Parser UserChoice
-parseQIRH
-  = foldl1 (<|>) $ map (\q -> try $ uncurry generiqparse (fn q, q)) [Quit, Inspect, Reset, Help]
-  where
-    fn = ((\(fs:rst) -> (toLower fs):rst) . show)
-
-parseVerbose = do
-  _ <- char ':'
-  _ <- string "verbose"
-  spaces
-  opt <- try (string "on") <|> try (string "off")
-  pure $ Verbose $ case opt of
-    "on"  -> True
-    "off" -> False
-    _     -> error "Invalid option"
-
-parseMode = do
-  _ <- char ':'
-  _ <- string "mode"
-  spaces
-  opt <- try (string "normal") <|> try (string "slow")
-  pure $ Mode $ case opt of
-    "normal" -> True
-    "slow"   -> False
-    _        -> error "Invalid option"
-
-parseImport = do
-  _ <- char ':'
-  _ <- string "import"
-  spaces
-  path <- many (noneOf [])
-  pure $ Import path
-
-parseBFCode, parseSlowerExecution :: Parser UserChoice
-parseSlowerExecution = do
-  _ <- char ':'
-  _ <- string "slow-exec"
-  _ <- spaces
-  number <- many (oneOf ['0'..'9']) <|> string ""
-  _ <- spaces
-  bf <- many (noneOf [])
-  pure . SlowedExecution $
-    case (parseCode bf, number) of
-      (Left _,      _) -> Left NotDelayed
-      (Right code, "") -> Right (0 , code)
-      (Right code,  _) -> Right (read number , code)
-
-
-parseBFCode = parseIntruction >>= pure . Code
-parseUserChoice :: [Char] -> Either ParseError UserChoice
-parseUserChoice = parse (
-                   try parseQuit
-               <|> try parseSlowerExecution
-               <|> try parseReset
-               <|> try parseMode
-               <|> try parseInspect
-               <|> try parseHelp
-               <|> try parseImport
-               <|> try parseVerbose
-               <|> try parseBFCode
-              ) ""
-
-
-parseOptHelp :: Parser Opt
-parseOptHelp = string "--help" >> return OptHelp
-
-parseOptFile :: Parser Opt
-parseOptFile = do
-  _ <- string "--file"
-  _ <- spaces
-  fileName <- many (noneOf [])
-  return $ File fileName
-
-parseOpt :: String -> Either ParseError Opt
-parseOpt = parse (try parseOptHelp <|> try parseOptFile) ""
 
 helloWorld :: String
 helloWorld = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
---
+++ src/Parser.hs
@@ -0,0 +1,140 @@
+module Parser where
+
+import Data.Char
+import Text.ParserCombinators.Parsec
+import qualified Data.List.Zipper as Z
+
+data BFExpr = Increment | Decrement
+            | GoForward | GoBackward
+            | Input     | Output
+            | Loop [BFExpr] deriving (Show, Eq)
+
+data NotDelayed = NotDelayed deriving (Show, Eq)
+
+data UserChoice = Quit
+                | Reset         | Help
+                | Inspect       | Verbose Bool
+                | Mode Bool
+                | SlowedExecution (Either NotDelayed (Int, [BFExpr]))
+                | Code [BFExpr] | Import FilePath
+                deriving (Show, Eq)
+
+ 
+data Opt = OptHelp | File FilePath
+
+
+type BFState = Z.Zipper Integer
+
+-- Parsing stuff
+symbols :: String
+symbols = "+-<>,."
+
+instructions :: [BFExpr]
+instructions = [
+  Increment,
+  Decrement,
+  GoBackward,
+  GoForward,
+  Input,
+  Output
+ ]
+
+parseIntruction :: Parser [BFExpr]
+parseIntruction =
+  many $
+     foldl1 (<|>) (fn:
+                 zipWith (\a b -> char a >> return b) symbols instructions)
+  where
+    fn :: Parser BFExpr
+    fn = char '[' >> parseIntruction >>= \ins -> char ']' >> return (Loop ins)
+
+
+parseCode :: String -> Either ParseError [BFExpr]
+parseCode
+  = parse parseIntruction "code.bf"
+
+generiqparse :: String -> UserChoice -> Parser UserChoice
+generiqparse str out
+  = char ':' >> string str >> pure out
+
+parseVerbose, parseInspect, parseHelp, parseQuit, parseReset, parseImport :: Parser UserChoice
+parseQuit    = generiqparse "quit"    Quit
+parseInspect = generiqparse "inspect" Inspect
+parseReset   = generiqparse "reset"   Reset
+parseHelp    = generiqparse "help"    Help
+
+parseQIRH :: Parser UserChoice
+parseQIRH
+  = foldl1 (<|>) $ map (\q -> try $ uncurry generiqparse (fn q, q)) [Quit, Inspect, Reset, Help]
+  where
+    fn = ((\(fs:rst) -> (toLower fs):rst) . show)
+
+parseVerbose = do
+  _ <- char ':'
+  _ <- string "verbose"
+  spaces
+  opt <- try (string "on") <|> try (string "off")
+  pure $ Verbose $ case opt of
+    "on"  -> True
+    "off" -> False
+    _     -> error "Invalid option"
+
+parseMode = do
+  _ <- char ':'
+  _ <- string "mode"
+  spaces
+  opt <- try (string "normal") <|> try (string "slow")
+  pure $ Mode $ case opt of
+    "normal" -> True
+    "slow"   -> False
+    _        -> error "Invalid option"
+
+parseImport = do
+  _ <- char ':'
+  _ <- string "import"
+  spaces
+  path <- many (noneOf [])
+  pure $ Import path
+
+parseBFCode, parseSlowerExecution :: Parser UserChoice
+parseSlowerExecution = do
+  _ <- char ':'
+  _ <- string "slow-exec"
+  _ <- spaces
+  number <- many (oneOf ['0'..'9']) <|> string ""
+  _ <- spaces
+  bf <- many (noneOf [])
+  pure . SlowedExecution $
+    case (parseCode bf, number) of
+      (Left _,      _) -> Left NotDelayed
+      (Right code, "") -> Right (0 , code)
+      (Right code,  _) -> Right (read number , code)
+
+
+parseBFCode = parseIntruction >>= pure . Code
+parseUserChoice :: [Char] -> Either ParseError UserChoice
+parseUserChoice = parse (
+                   try parseQuit
+               <|> try parseSlowerExecution
+               <|> try parseReset
+               <|> try parseMode
+               <|> try parseInspect
+               <|> try parseHelp
+               <|> try parseImport
+               <|> try parseVerbose
+               <|> try parseBFCode
+              ) ""
+
+
+parseOptHelp :: Parser Opt
+parseOptHelp = string "--help" >> return OptHelp
+
+parseOptFile :: Parser Opt
+parseOptFile = do
+  _ <- string "--file"
+  _ <- spaces
+  fileName <- many (noneOf [])
+  return $ File fileName
+
+parseOpt :: String -> Either ParseError Opt
+parseOpt = parse (try parseOptHelp <|> try parseOptFile) ""
